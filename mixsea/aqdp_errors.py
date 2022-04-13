59d70e887c2ddbebdc6c027f1eae977d74f0b446
inciente <nogutier@ucsd.edu>
2022-04-12 22:19:45 -0700

    wrote function make_table that summarizes ensemble

--- mixsea/aqdp_errors.py
+++ mixsea/aqdp_errors.py
@@ -1,8 +1,8 @@
 import numpy as np; import xarray as xr; 
 import os, sys, importlib, random; 
 import pandas as pd; 
-
-import load_aqdp; 
+#from parfor import parfor
+import load_aqdp;
 
 
 
@@ -10,25 +10,28 @@ def make_table(turb_method, indices):
     ''' Make a table of data describing the performance of 
      different a turbulent method for a set of indices.
     '''
-    ens = turb_method.ensembles; # ensembles associated with method
-    motion = load_aqdp.vehicle_motion(ens);  
-    all_pressures = ens.get_pressure().values; # depth 
-    # --- create dataframe in which to save data
-    table = pd.DataFrame( \
-            columns=['epsilon','noise','pressure','vert_vel'])
+    # --- define class instances associated with turb_method
+    my_ens = turb_method.ensembles; 
+    my_motion = load_aqdp.vehicle_motion(my_ens);  
+    all_pressures = my_ens.get_pressure().values; # depth 
+    # --- create lists to save output
+    epsi = []; noise = []; dpdt = []; 
     # --- roll through and append one row per index
+ #   @parfor
     for index in indices:
-        method_output = turb_method.estimate_epsilon(index); 
-        vert_vel = motion.vertical_velocity( index ).values; 
-        # organize data into a dictionary
-        data_2_save = { \
-                'time':[ens.time[index]],
-                'epsilon':[method_output[0]],
-                'noise':[method_output[1]], 
-                'pressure':[all_pressures[index]],
-                'vert_vel':[vert_vel] }
-        data_as_row = pd.DataFrame.from_dict( data=data_2_save ); 
-        table = table.append( data_as_row ); 
-    table = table.set_index('time'); 
+        method_output = turb_method.estimate_epsilon(index);
+        vert_vel = my_motion.vertical_velocity( index ).values; 
+        # save output
+        epsi.append(method_output[0]); 
+        noise.append(method_output[1]);
+        dpdt.append( vert_vel );
+    #table = {'epsilon':epsi, 'noise':noise,
+    #        'vert_vel':dpdt, 'depth':all_pressures[indices],
+    #        'time':ens.time[indices],'index':indices};
+    table = pd.DataFrame( data={ \
+             'epsilon':epsi, 'noise':noise,
+             'vert_vel':dpdt, 'depth':all_pressures[indices],
+             'time':my_ens.time[indices] }, 
+             index=indices)
     return table
 
